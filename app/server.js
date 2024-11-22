d87e441a99f20108d16cc9822e82cd2cbd11e7ef
Halima <just.hvli@gmail.com>
2024-11-22 18:21:07 +0100

    init:initialize project with dependencies + structure

---
+++ app/server.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something went wrong!');
+});
+
+app.use(express.json());
+
+app.listen(port, () => {
+    console.log(`Server is running on port http://localhost:${port}`)
+})
---
+++ client/src/app/massages/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+    return (
+        <div>
+            <h1>Massages</h1>
+        </div>
+    );
+}

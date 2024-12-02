20e508d0cd480b457f8fefca34b3a576a9016e07
Halima <just.hvli@gmail.com>
2024-12-02 14:35:45 +0100

    Add: app + client folders

---
+++ app/controllers/timeSlotController.js
@@ -0,0 +1,12 @@
+const TimeSlot = require('../models/TimeSlot');
+
+
+exports.getAvailableTimeSlots = async (req, res) => {
+    const { date } = req.params;
+
+    try {
+        const timeSlots = await timeSlots.fin
+    } catch (error) {
+
+    }
+}
